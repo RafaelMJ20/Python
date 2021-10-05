00572c3ff8eaf498753b4ec7c588c5939af70aff
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2021-10-05 15:58:52 -0500

    Add files via upload

---
+++ Hola_Mundo.py
@@ -0,0 +1,9 @@
+# Hola Mundo en Python
+# Title         :Hola Mundo en Python
+# Description   :Primer Hola Mundo en Python desde vim en Windows
+# Author        :Rafael Miranda Jimenez
+# Date          :20211005
+# Version       :1
+# Usage         :c:\>python /path/Hola_Mundo.py
+
+print ("Primer hola mundo en Python")
